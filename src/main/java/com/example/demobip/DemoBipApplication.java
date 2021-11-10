c2d18b592f36f096f5c62a1e291fd14fb4338228
fabrizia.binetti <fabrizia@BIPFBINETTI03.bip.lan>
2021-11-10 15:56:37 +0100

    first commit

---
+++ src/main/java/com/example/demobip/DemoBipApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demobip/DemoBipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
